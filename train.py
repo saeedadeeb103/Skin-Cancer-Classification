9e2bc4fdb15d139a4ec3f1a56d842bae50462da8
saeed al-maqtari <saeed.al-maqtari@utn.de>
2025-02-18 22:27:19 +0100

    updates to the encoder

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ from pytorch_lightning.callbacks import EarlyStopping
 import hydra
 from datasets import list_datasets, get_dataset_by_name
 from encoders.encoders import timm_backbones
-from torchaudio import transforms as T
 from hydra.core.hydra_config import HydraConfig
+from utils.helper_functions import collate_fn
 import numpy as np
 import random
 import matplotlib.pyplot as plt
